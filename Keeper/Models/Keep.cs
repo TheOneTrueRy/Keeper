d8628573c605117cbe133974e528ceac1014ddbf
Ryan Wittman <OfficiallyRy@outlook.com>
2023-03-31 10:57:18 -0600

    Made all my back-end models hopefully correctly :)

---
+++ Keeper/Models/Keep.cs
@@ -0,0 +1,19 @@
+namespace Keeper.Models
+{
+  public class Keep
+  {
+    public int Id { get; set; }
+    public string CreatorId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Img { get; set; }
+    public int? Views { get; set; }
+    public int KeptCount { get; set; }
+    public Profile Creator { get; set; }
+  }
+
+  public class VaultedKeep : Keep
+  {
+    public int VaultkeepId { get; set; }
+  }
+}
