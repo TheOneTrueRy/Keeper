ef334b29e888cb412b607e4c1b834bc405f96375
Ryan Wittman <OfficiallyRy@outlook.com>
2023-03-31 10:29:46 -0600

    Let's begin.

---
+++ Keeper.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:7045' : ''
+export const useSockets = false
+export const domain = 'theonetruery.us.auth0.com'
+export const clientId = 'ALP2sxGR4JVMedqc5JHsI8JrcCjavCgn'
+export const audience = 'https://big-bango-bongo.com'
