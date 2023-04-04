d3729682dc2e52bf2a1b1889eb4e4393a2dd2774
Ryan Wittman <OfficiallyRy@outlook.com>
2023-04-04 09:12:22 -0600

    Keep Cards are now fancy instead of gross :)

--- Keeper.client/src/components/KeepCard.vue
+++ Keeper.client/src/components/KeepCard.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="border border-dark border-1 rounded elevation-2">
-    <img :src="keep.img" :alt="keep.name" :title="keep.name" class="rounded-top img-fluid selectable"
+    <img :src="keep.img" :alt="keep.name" :title="keep.name" class="rounded-top img-fluid selectable bing"
       onerror="this.src='broken-image.png'" data-bs-toggle="modal" data-bs-target="#keepDetails"
       @click="setKeep(keep.id)">
-    <div class="d-flex justify-content-between align-items-center p-1">
-      <span class="fs-5 hover" data-bs-toggle="modal" data-bs-target="#keepdetails" @click="setKeep(keep.id)">{{ keep.name
-      }}</span>
+    <div class="d-flex justify-content-between align-items-center p-1 bong text-light">
+      <span class="fs-4 text-shadow hover" data-bs-toggle="modal" data-bs-target="#keepdetails"
+        @click="setKeep(keep.id)">{{ keep.name
+        }}</span>
       <slot></slot>
     </div>
   </div>
@@ -49,5 +50,19 @@ export default {
 
 .hover:hover {
   transform: scale(1.05);
+}
+
+.text-shadow {
+  text-shadow: 1px 1px 0 black;
+}
+
+.bing {
+  position: relative;
+}
+
+.bong {
+  position: absolute;
+  transform: translateY(-50px);
+  width: 23vw;
 }
 </style>
