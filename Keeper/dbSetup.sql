ff6849cfd1e692deb186e59be5c32f449b4d2f58
Ry <OfficiallyRy@outlook.com>
2023-05-29 15:58:36 -0600

    Changed database and mySQL server to new one

--- Keeper/dbSetup.sql
+++ Keeper/dbSetup.sql
@@ -1,4 +1,4 @@
--- Active: 1682721609011@@SG-vine-marlin-4985-7518-mysql-master.servers.mongodirector.com@3306@marlin
+-- Active: 1685397470514@@SG-boom-cinema-6923-7571-mysql-master.servers.mongodirector.com@3306@Sandbox
 CREATE TABLE IF NOT EXISTS accounts(
   id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
   createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
