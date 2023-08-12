20b3420fd7eefbe9d66e1647afa69ff1086e798c
Ry <OfficiallyRy@outlook.com>
2023-08-11 22:04:57 -0600

    Build for small dropdown style fixes

---
+++ Keeper/wwwroot/assets/ProfilesService.17afe8ce.js
@@ -0,0 +1,1 @@
+import{j as a,A as t}from"./index.c17b0bdb.js";class i{async getProfile(e){const s=await a.get("api/profiles/"+e);t.profile=s.data}async getUsersKeeps(e){const s=await a.get(`api/profiles/${e}/keeps`);t.keeps=s.data}async getUsersVaults(e){const s=await a.get(`api/profiles/${e}/vaults`);t.vaults=s.data}}const o=new i;export{o as p};
